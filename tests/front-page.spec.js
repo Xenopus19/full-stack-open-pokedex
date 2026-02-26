f254279b344d1f28173529d3f06f51040f4f488b
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-02-26 15:13:09 +0200

    Pokemon page can be opened test added

--- tests/front-page.spec.js
+++ tests/front-page.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be opened', async ({ page }) => {
+     await page.goto('http://localhost:8080')
+     await page.getByText('ivysaur').click();
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText(/chlorophyl/i)).toBeVisible()
+    await expect(page.getByText(/overgrow/i)).toBeVisible()
+  })
 })
